a2fcb9d426daee3256c3def1e3b47941687733f4
Marcin Kozlowski <marcinguy@gmail.com>
2022-06-19 19:42:06 +0200

    Typo

--- checkmate/scripts/manage.py
+++ checkmate/scripts/manage.py
@@ -76,7 +76,7 @@ def load_command_class(settings):
 def main():
 
     if not os.path.isdir('.git'):
-      sys.stderr.write("Not a git repository.\nTry running: \"git init && git add. && git commit -m \"init\" \" in the folder\n")
+      sys.stderr.write("Not a git repository.\nTry running: \"git init && git add . && git commit -m \"init\" \" in the folder\n")
       exit(-1)
     if not "CODE_DIR" in os.environ:
       sys.stderr.write("CODE_DIR env not set. Please set it to directory of your code.\nTry running: \"export CODE_DIR=/path/to/code\" before\n")
