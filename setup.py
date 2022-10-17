ebda608246d8719ee966533c7562b64afcddb221
Marcin Kozlowski <marcinguy@gmail.com>
2022-10-17 18:28:24 +0200

    Fix

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ author=u'Andreas Dewes - QuantifiedCode UG (haftungsbeschränkt)',
 author_email = 'andreas@quantifiedcode.com',
 license = 'MIT',
 install_requires = [
-'blitzdb3-ce',
 'pylint',
 'pyflakes',
 'pep8',
