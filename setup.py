35cf9385f5d1907bc33bc24e86396da1603dab4e
Marcin Kozlowski <marcinguy@gmail.com>
2022-10-17 16:47:00 +0200

    Changed urls

--- setup.py
+++ setup.py
@@ -7,22 +7,23 @@ version='0.2.0',
 author=u'Andreas Dewes - QuantifiedCode UG (haftungsbeschränkt)',
 author_email = 'andreas@quantifiedcode.com',
 license = 'MIT',
-install_requires = """
-pylint
-pyflakes
-pep8
-six
-chardet
-blitzdb
-pyyaml
-sqlalchemy
-""",
+install_requires = [
+"blitzdb@https://github.com/marcinguy/blitzdb3-ce"],
+'pylint',
+'pyflakes',
+'pep8',
+'six',
+'chardet',
+'blitzdb',
+'pyyaml',
+'sqlalchemy'
+],
 entry_points = {
         'console_scripts': [
                'checkmate = checkmate.scripts.manage:main',
         ],
     },
-url='https://github.com/quantifiedcode/checkmate',
+url='https://github.com/marcinguy/checkmate-ce',
 packages=find_packages(),
 zip_safe = False,
 description='A meta-code checker written in Python.',
@@ -34,12 +35,12 @@ to provide clear, actionable insights to the user.
 Documentation
 =============
 
-The documentation can be found `here <https://docs.quantifiedcode.com/checkmate>`.
+The documentation can be found `here <https://github.com/marcinguy/checkmate-ce>`.
 
 Source Code
 ===========
 
-The source code can be found on `Github <https://github.com/quantifiedcode/checkmate>`.
+The source code can be found on `Github <https://github.com/marcinguy/checkmate-ce>`.
 
 Changelog
 =========
