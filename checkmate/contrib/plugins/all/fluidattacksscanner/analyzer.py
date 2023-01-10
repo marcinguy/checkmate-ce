e3277bfd798ab20ec1b83ee49b585b5268194757
Marcin Kozlowski <marcinguy@gmail.com>
2023-01-10 10:08:41 +0100

    Fix

--- checkmate/contrib/plugins/all/fluidattacksscanner/analyzer.py
+++ checkmate/contrib/plugins/all/fluidattacksscanner/analyzer.py
@@ -69,7 +69,10 @@ class FluidAttacksAnalyzer(BaseAnalyzer):
 
             my_file = open(fresults.name, 'r')
             reader = csv.reader(my_file)
-            next(reader)
+            try:
+              next(reader)
+            except:
+              pass
 
             outjson = []
             val ={}
