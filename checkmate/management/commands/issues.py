2ad0906c32d67557959586ae123404b9277b230e
Marcin Kozlowski <marcinguy@gmail.com>
2023-02-23 11:50:02 +0100

    Fix

--- checkmate/management/commands/issues.py
+++ checkmate/management/commands/issues.py
@@ -96,7 +96,7 @@ class Command(BaseCommand):
           console.print(table)
           print("[bold red]Note:[/bold red] Issues include findings accross all revisions i.e it can be that you have fixed it in your latest revision, but the finding will still appear here (for the affected revision)")
 
-          print("This scan could be 4x+ faster with PRO version :thumbs_up: https://www.betterscan.io/pricing")
+          print("This scan could be 8-10x+ faster with PRO version :thumbs_up: https://www.betterscan.io/pricing")
 
           #for issue in issues:
           #    print(("%(analyzer)s\t%(code)s\t" % {'analyzer': issue['analyzer'],
