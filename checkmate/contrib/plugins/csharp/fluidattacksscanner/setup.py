c51f913441ef2ec2a663a8052cdbee3ddc21ef5a
Marcin Kozlowski <marcinguy@gmail.com>
2023-05-15 09:37:03 +0200

    Adjustments
    Signed-off-by: Marcin Kozlowski <marcinguy@gmail.com>

--- checkmate/contrib/plugins/csharp/fluidattacksscanner/setup.py
+++ checkmate/contrib/plugins/csharp/fluidattacksscanner/setup.py
@@ -2,10 +2,10 @@ from .analyzer import FluidAttacksAnalyzer
 from .issues_data import issues_data
 
 analyzers = {
-    'fluidattacks':
+    'fluidattackscsharp':
         {
-            'name': 'fluidattacks',
-            'title': 'fluidattacks',
+            'name': 'fluidattackscsharp',
+            'title': 'fluidattackscsharp',
             'class': FluidAttacksAnalyzer,
             'language': 'csharp',
             'issues_data': issues_data,
