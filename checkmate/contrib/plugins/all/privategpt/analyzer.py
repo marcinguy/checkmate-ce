b83907802f787269333ebaaf29f228113b81c57c
Marcin Kozlowski <marcinguy@gmail.com>
2023-05-21 15:30:18 +0200

    Adjustments
    Signed-off-by: Marcin Kozlowski <marcinguy@gmail.com>

--- checkmate/contrib/plugins/all/privategpt/analyzer.py
+++ checkmate/contrib/plugins/all/privategpt/analyzer.py
@@ -52,18 +52,20 @@ class PrivateGptAnalyzer(BaseAnalyzer):
 
                 prompt="I want you to act as a security engineer. Your task is to security review the code and find potential security bugs.\n    Your input would be a git diff, please only give suggestion on only the edited content. Consider the context for better suggestion.\n    Find and fix any bugs and typos. If no bug is found, just output \\\"No obvious bug found.\\\"\n    Do not include any personal opinions or subjective evaluations in your response.\n    Your output should looks like:\n\n      [\n        {\n        \\\"line\\\": 66,\n        \\\"finding\\\":\\\"...(your suggestion)\\\"\n        },\n        {\n        \\\"line\\\": 77,\n        \\\"finding\\\":\\\"...(your suggestion)\\\"\n        }\n\n     ]"
                 
-               jsondata={}
-               #open text file in read mode
-               filecode = open(f.name, "r")
+                jsondata={}
+                #open text file in read mode
+                filecode = open(f.name, "r")
 
-               code  = filecode.read()
-               filecode.close()
+                code  = filecode.read()
+                filecode.close()
   
-               jsondata["question"] = prompt+"\n"+code
+                jsondata["question"] = prompt+"\n"+code
 
-               x = requests.post(url, json = jsondata)
-               result = x.text
+                x = requests.post(url, json = jsondata)
+                result = x.content
 
+            except:
+                pass
 
 
             try:
@@ -93,4 +95,5 @@ class PrivateGptAnalyzer(BaseAnalyzer):
         finally:
             pass
         return {'issues': issues}
+
 
